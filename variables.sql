0641416f1b3e9e983b981eae4057e827dcdcd404
Kenneth Adjei-mensah <kenadjei20@gmail.com>
2023-07-06 11:23:05 +0100

    variable sql update

--- variables.sql
+++ variables.sql
@@ -1,5 +1,42 @@
 -- VARIABLES
- DECLARE @Name VARCHAR(20)= 'Gall Erickson';
- Declare @No nvarchar(11) = 2345;
+DECLARE @Name VARCHAR(20) = 'Gall Erickson';
+DECLARE @No NVARCHAR(11) = '2345';
 
- SELECT * FROM ContactDetails WHERE fullname = @Name and PhoneNumber = @No 
+SELECT *
+FROM ContactDetails
+WHERE FullName = @Name
+  AND PhoneNumber = @No;
+
+-- Declare variables
+DECLARE @PersonName VARCHAR(100),
+        @PhoneNumber NVARCHAR(20);
+
+-- Check if cursor exists and close it if open
+IF CURSOR_STATUS('global', 'MTN_CUSTOMERS') >= 0
+BEGIN
+    CLOSE MTN_CUSTOMERS;
+    DEALLOCATE MTN_CUSTOMERS;
+END;
+
+-- Declare cursor
+DECLARE MTN_CUSTOMERS CURSOR FOR
+SELECT FullName,
+       PhoneNumber
+FROM ContactDetails;
+
+OPEN MTN_CUSTOMERS;
+
+PRINT '    MTN CUSTOMERS';
+PRINT 'Customer Name     ' + '  Customer Phone Numbers';
+
+-- Declare variables before the WHILE loop
+FETCH NEXT FROM MTN_CUSTOMERS INTO @PersonName, @PhoneNumber;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    PRINT @PersonName + '     ' + @PhoneNumber;
+    FETCH NEXT FROM MTN_CUSTOMERS INTO @PersonName, @PhoneNumber;
+END;
+
+CLOSE MTN_CUSTOMERS;
+DEALLOCATE MTN_CUSTOMERS;
